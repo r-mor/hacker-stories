6f2eea4886817f8ddf60e3bae394e6548f614553
Randl Morales <morales.randl@gmail.com>
2023-04-02 23:45:42 -0400

    props handling

--- src/App.jsx
+++ src/App.jsx
@@ -1,100 +1,116 @@
 import * as React from 'react';
 
-const title = 'World';
 
-const numbers = [1,2,3,4,5,6,7];
+const App = () => {
+  
+  const [searchTerm, setSearchTerm] = React.useState(localStorage.getItem('search') || 'World');
 
-const exponentialNumbers = numbers.map(function (number) {
-  return number * number;
-});
+  const boardGameList = [
+    {
+      title: 'Deception Murder in Hong Kong',
+      url: 'https://www.boardgames.ca/deception-murder-in-hong-kong-board-game.html',
+      designer: null,
+      publisher: 'Grey Fox Games',
+      score: 9.5,
+      objectId: 1
+    },
+    {
+      title: 'Cosmic Encounter',
+      url: 'https://www.fantasyflightgames.com/en/products/cosmic-encounter/',
+      designer: null,
+      publisher: 'Fantasy Flight Games',
+      score: 10.0,
+      objectId: 2
+    },
+    {
+      title: 'Dominion',
+      url: 'https://www.riograndegames.com/games/dominion/',
+      designer: null,
+      publisher: 'Rio Grande Games',
+      score: 8.5,
+      objectId: 3
+    },
+    {
+      title: 'Ark Nova',
+      url: 'https://capstone-games.com/board-games/ark-nova/',
+      designer: null,
+      publisher: 'Capstone Games',
+      score: 9.0,
+      objectId: 4
+    },
+    {
+      title: 'Mindbug',
+      url: 'https://mindbug.me/',
+      designer: null,
+      publisher: 'Nerdlab Games',
+      score: 8.5,
+      objectId: 5
+    },
+  ];
 
-console.log("Hello " + exponentialNumbers);
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value);
 
-const boardGameList = [
-  {
-    title: 'Deception Murder in Hong Kong',
-    url: 'https://www.boardgames.ca/deception-murder-in-hong-kong-board-game.html',
-    designer: null,
-    publisher: 'Grey Fox Games',
-    score: 9.5,
-    objectId: 1
-  },
-  {
-    title: 'Cosmic Encounter',
-    url: 'https://www.fantasyflightgames.com/en/products/cosmic-encounter/',
-    designer: null,
-    publisher: 'Fantasy Flight Games',
-    score: 10.0,
-    objectId: 2
-  },
-  {
-    title: 'Dominion',
-    url: 'https://www.riograndegames.com/games/dominion/',
-    designer: null,
-    publisher: 'Rio Grande Games',
-    score: 8.5,
-    objectId: 3
-  },
-  {
-    title: 'Ark Nova',
-    url: 'https://capstone-games.com/board-games/ark-nova/',
-    designer: null,
-    publisher: 'Capstone Games',
-    score: 9.0,
-    objectId: 4
-  },
-  {
-    title: 'Mindbug',
-    url: 'https://mindbug.me/',
-    designer: null,
-    publisher: 'Nerdlab Games',
-    score: 8.5,
-    objectId: 5
-  },
-];
+    localStorage.setItem('search', event.target.value);
+  }
 
-const App = () => (
+  const searchedBoardGames = boardGameList.filter( 
+    story => story.title.toLocaleLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  return (
     <>
-      <h1>Hello {title}</h1>
-      <Search />
-      <BoardGamesList />      
+      <h1>Hello {searchTerm}</h1>
+      <Search onSearch={handleSearch} searchTerm={searchTerm}/>
+      
+      <BoardGamesList list={searchedBoardGames}/>      
       <hr />
     </>
-)
+)}
 
-const BoardGamesList = () => (
+const BoardGamesList = ({list}) => (
     <>
       <h2>Here are some of my favorite board games:</h2>
       <ul>
-      {boardGameList.map((item) =>
-        <li key={item.objectId}>
-          <span>
-            <a href={item.url}>{item.title}</a>
-          </span>
-          <span> by {item.publisher}</span>
-          <span> score: {item.score}</span>
-        
-        </li>
-        )}
+      {list.map(({objectId, ...item}) =>
+        <ListItem key={objectId} 
+        {...item}
+        />
+      )}
       </ul>
-    </>
+  </>
   )
 
+const ListItem = (
+  { 
+    
+    title, 
+    url, 
+    publisher, 
+    score
+    
+  }
+  ) => (
+  <>
+    <li>
+      <span>
+        <a href={url}>{title}</a>
+      </span>
+      <span> by {publisher}</span>
+      <span> score: {score}</span>
+    </li>
+  </>
+)
 
-const Search = () => {
-  const handleChange = (event) => {
-    //synthetic event
-    console.log(event);
-    // value of target (here: input HTML element)
-    console.log(event.target.value);
-  };
+const Search = ({searchTerm, onSearch}) => (
+  <>
+    <label htmlFor="search">Search: </label>
+    <input id="search" value={searchTerm} type="text" onChange={onSearch}/>
 
-  return(
-    <>
-      <label htmlFor="search">Search: </label>
-      <input id="search" type="text" onChange={handleChange}/>
-    </>
-  )
-}
+    <p>
+      Searching for <strong>{searchTerm}</strong>
+    </p>
+  </>
+)
 
 export default App;
